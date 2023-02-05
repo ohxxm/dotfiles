5c0eece02376222f2072746f74b2b06109fec3fd
Ohm <ohmrc2545@gmail.com>
2023-02-05 12:16:17 -0500

    updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,5 @@
 set -U fish_greeting
 starship init fish | source
-fetch -c ~/.config/hypr/apps/fetch/conf
+fetch -c ~/.config/bspwm/apps/fetch/conf
 
 alias ls="exa --icons -l"
-
-if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx /bin/bspwm -- -keeptty
-    end
-end
